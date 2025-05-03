5724755c2908caf615ac6a3836500380be56fd69
clyle342 <clyle342@gmail.com>
2025-05-03 12:56:18 +0300

    new_user_script

---
+++ linux-basics/shell_scripting/new_user.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+
+#a shell script that creates another user
+
+read -p "Enter new username: " username
+
+sudo adduser $username
+
+echo usermod -aG sudo $username
+
+echo "User $username created and added to sudo group."
+
+#the created user will have adminstrative priviledges since we have given the sudo
+
